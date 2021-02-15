d96d47b7fc6566a78fcf1f9c79072dfcb9197b36
Alejogb1 <alejogarciag1@gmail.com>
2021-02-15 16:07:19 -0300

    fetch and reducer
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

--- app/src/Pages/Products/Products.js
+++ app/src/Pages/Products/Products.js
@@ -1,10 +1,25 @@
 import PropTypes from "prop-types"
 import { propTypes } from "react-bootstrap/esm/Image"
-import { productsReducer, initialState } from "./../../reducers/products";
+import { productsReducer, initialState } from "./../../reducers/product";
 import { FETCHING, FETCH_SUCCESS } from "./../../reducers/actions/products";
-
+import {useEffect, useReducer} from "react"
+import axios from "axios"
 const Products = ({search}) => { 
-    console.log(search)
+    
+    const [state, dispatch] = useReducer(productsReducer, initialState)
+
+    const getProducts = async () => {
+        try {
+            const {data} = await axios.get(
+                `https://api.mercadolibre.com/sites/MLA/search?q=${search}&limit=8`
+            )
+        } catch (e) {
+            console.error(e)
+        } 
+    }
+    useEffect(() => {      
+        getProducts() 
+    }, [search])
     return(
         <h1>Productos a traer {search}</h1>
     )
@@ -13,4 +28,4 @@ const Products = ({search}) => {
 Products.propTypes = {
     search: PropTypes.string,
 }
-export default Products
+export default Products 
