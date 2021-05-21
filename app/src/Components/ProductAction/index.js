1677f031b2be49647657d34ea83226b54f534181
Alejogb1 <52103963+Alejogb1@users.noreply.github.com>
2021-05-21 17:52:43 -0300

    ADD: Free shipping conditional

--- app/src/Components/ProductAction/index.js
+++ app/src/Components/ProductAction/index.js
@@ -18,53 +18,55 @@ import {
   ShieldIcon,
 } from "./style";
 
-const ProductAction = () => {
+const ProductAction = ({title, condition, price, free_shipping}) => {
   return (
     <Container>
-      <Condition>Novo</Condition>
+      <Condition>{condition}</Condition>
 
       <Row>
-        <h1>Camiseta Branca da Marca Desconhecida</h1>
+        <h1>{title}</h1>
         <HeartIcon />
       </Row>
-
-      <DispatchTag>Enviando normalmente</DispatchTag>
+      <DispatchTag>Envíos normales</DispatchTag>
 
       <PriceCard>
         <PriceRow>
-          <span className="symbol">R$</span>
-          <span className="fraction">34</span>
-          <span className="cents">99</span>
+          <span className="symbol">ARS$</span>
+          <span className="fraction">{price}</span>
+          <span className="cents">00</span>
         </PriceRow>
 
-        <InstallmentsInfo>em 3x de R$ 11,67</InstallmentsInfo>
+        <InstallmentsInfo>en 3 cuotas de ${(price/3).toFixed()}</InstallmentsInfo>
       </PriceCard>
 
-      <StockStatus>Estoque disponível</StockStatus>
+      <StockStatus>Disponible</StockStatus>
 
       <MethodCard>
-        <CheckIcon />
-
-        <div>
-          <span className="title">Frete grátis</span>
-          <span className="details">Benefício Lorem Ipsum</span>
-          <a href="#" className="more">
-            Ver mais opções
-          </a>
-        </div>
+        {free_shipping === true ? (
+          <> 
+            <CheckIcon />
+            <div>
+              <span className="title">Envío gratis a todo el país</span>
+              <span className="details">Benefício Mercado Envíos</span>
+              <a href="#" className="more">
+                Ver más opciones
+              </a>
+            </div>
+          </>       
+        ) : (<></>)}
+       
       </MethodCard>
 
       <Actions>
-        <Button solid>Comprar agora</Button>
-        <Button>Adicionar ao carrinho</Button>
+        <Button solid>Comprar ahora</Button>
+        <Button>Agregar al carrito</Button>
       </Actions>
 
       <Benefits>
         <li>
           <ShieldIcon />
           <p>
-            Compra Garantida, receba o produto que está esperando ou devolvemos
-            seu dinheiro.
+          Compra Protegida, recibí el producto que esperabas o te devolvemos tu dinero.
           </p>
         </li>
       </Benefits>
