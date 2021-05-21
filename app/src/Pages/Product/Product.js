3397b5b9659c56800018ec5e6a23064cd5ef1186
Alejogb1 <52103963+Alejogb1@users.noreply.github.com>
2021-05-21 17:55:04 -0300

    ADD: Styles and products layout

--- app/src/Pages/Product/Product.js
+++ app/src/Pages/Product/Product.js
@@ -7,37 +7,51 @@ import {Container, Wrapper, Row,
 import {useParams} from "react-router-dom"
 import ProductAction from "../../Components/ProductAction/index";
 import SellerInfo from "../../Components/SellerInfo/index";
+import useProduct from "../../customHooks/useProduct"
 const Product = () => {
     const {id} = useParams()
-    return (
-        <Container>
-            <Wrapper>
-                <Container>
-                    <Row>
-                        <a href="#">asdad</a>
-                        <a href="#">Vendasdader uno igual</a>
-                    </Row>
-                </Container>
-                <Panel>
-                    <Column>
-                        <Gallery>
-                            <img alt="T-Shirt" />
-                        </Gallery>
-                         <Info />
-                    </Column>
-
-                    <Column>
-                        <ProductAction />
-                        <SellerInfo />
-
-                        <WarrantySection />
-                        <WarrantySection />
-                        <WarrantySection />
-                    </Column>
-                </Panel>
-            </Wrapper>
-        </Container>   
-    )
+    const [info] = useProduct(id)
+    console.log(info)
+    const product = info.product.information
+    console.log("PRODUCT", product)
+    if (info) {
+        return (
+            <>
+            {info.fetching ? (
+                <h3>Cargando...</h3>
+            ) : (
+                    <Container>
+                        <Wrapper>
+                            <Container>
+                                <Row>
+                                    <a href="#">Compartir</a>
+                                    <a href="#">Vender uno igual</a>
+                                </Row>
+                            </Container>
+                            <Panel>
+                                <Column>
+                                    <Gallery>
+                                        <img alt="T-Shirt" src={product.pictures[0].url} />
+                                    </Gallery>
+                                     <Info description={product.plain_text} />
+                                </Column>
+            
+                                <Column>
+                                    <ProductAction free_shipping={product.shipping.free_shipping} price={product.base_price} condition={product.condition} title={product.title} />
+                                    <SellerInfo  state={product.seller_address.state.name} city={product.seller_address.city.name} />
+            
+                                    <WarrantySection />
+                                    <WarrantySection />
+                                    <WarrantySection />
+                                </Column>
+                            </Panel>
+                        </Wrapper>
+                    </Container>   
+            )}
+            </>
+        )
+       
+    }
 }
 const WarrantySection = () => (
     <Section>
@@ -45,61 +59,27 @@ const WarrantySection = () => (
   
       <div>
         <span>
-          <p className="title">Compra Garantida com o Lorem Ipsum</p>
+          <p className="title">Compra Garantízada</p>
           <p className="description">
-            Receba o produto que está esperando ou devolvemos seu dinheiro
+          Tu compra está protegida. Si hay algún problema o no recibes el producto tal como lo compraste, te devolvemos el dinero que pagaste.
           </p>
         </span>
         <span>
-          <p className="title">Garantia do vendedor</p>
-          <p className="description">sem garantia</p>
+          <p className="title">Garantía del vendedor</p>
+          <p className="description">Garantía de fábrica: 6 meses</p>
         </span>
       </div>
   
-      <a href="#">Saiba mais sobre garantia</a>
+      <a href="#">Conocer más sobre Compra Protegida</a>
     </Section>
   );
   
-  const Info = () => (
+  const Info = ({description}) => (
     <Description>
-      <h2>Descrição</h2>
+      <h2>Descripción</h2>
   
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et dolor
-        bibendum diam ullamcorper bibendum vitae a ex. Ut in ipsum lacus. Nullam
-        dictum nisi sem, sed sagittis arcu ultricies ut. Mauris sodales nisl
-        velit. Mauris quis velit at urna bibendum viverra. Pellentesque sodales
-        laoreet nunc, et blandit libero lobortis eget. Pellentesque pellentesque
-        sed mauris dictum porttitor.
-        <br />
-        <br />
-        Itens inclusos: <br />
-        - 1x LED <br />
-        - 1x LED <br />
-        - 1x LED <br />
-        - 1x LED <br />
-        - 1x LED <br />
-        <br />
-        Etiam semper a dui ac lacinia. Curabitur at nisi ut urna aliquet
-        venenatis. Nullam eget leo nec lectus euismod finibus vel in enim. Vivamus
-        sollicitudin nisi est, cursus rhoncus orci finibus in. Cras volutpat
-        sodales neque. Vestibulum ante ipsum primis in faucibus orci luctus et
-        ultrices posuere cubilia curae; Donec imperdiet lorem a efficitur viverra.
-        Donec consectetur orci id ex sollicitudin vulputate. Nulla eget sapien
-        tortor. Aenean iaculis pellentesque felis, facilisis molestie risus mollis
-        non. Proin tempus, nunc non pulvinar tristique, ligula risus porta odio,
-        non pulvinar elit nisi vitae velit. Morbi vulputate dolor eu tortor
-        iaculis ultricies. Aliquam accumsan aliquam cursus. Curabitur id egestas
-        dolor. Ut sed nulla vestibulum, condimentum lacus a, semper augue.
-        Vestibulum sagittis dui ut lorem scelerisque rhoncus. In id facilisis leo.
-        Sed porttitor faucibus fringilla. Aenean eget mi vitae est convallis
-        pretium eget sed quam. Phasellus purus magna, consequat id laoreet in,
-        blandit quis eros. Integer fringilla justo nisi, vitae vulputate erat
-        finibus et. Duis lacinia ultrices purus. Curabitur ipsum purus, lacinia ut
-        nisl nec, rutrum tristique ipsum. Duis luctus est sem, a laoreet quam
-        scelerisque eget. Phasellus justo ex, pellentesque et dignissim nec,
-        malesuada at justo. Cras porta vel lacus in ultricies. Curabitur id tempus
-        tellus. Pellentesque hendrerit massa sit amet vulputate varius.
+       {description}
       </p>
     </Description>
   );
--- app/src/Pages/Products/Products.js
+++ app/src/Pages/Products/Products.js
@@ -5,6 +5,8 @@ import { FETCHING, FETCH_SUCCESS } from "./../../reducers/actions/products";
 import {useEffect, useReducer} from "react"
 import {Link} from "react-router-dom"
 import axios from "axios"
+import BASE_URL from "../../constants/index"
+import "../../styles.css"
 const Products = ({search}) => { 
     
     const [state, dispatch] = useReducer(productReducer, initialState)
@@ -12,7 +14,7 @@ const Products = ({search}) => {
     const getProducts = async () => {
         try {
             const {data} = await axios.get(
-                `https://api.mercadolibre.com/sites/MLA/search?q=${search}&limit=8`
+                `${BASE_URL}/sites/MLA/search?q=${search}&limit=8`
             )
             console.log(data.results)
             dispatch({ type: FETCH_SUCCESS, payload: { data : data.results}})
@@ -25,18 +27,42 @@ const Products = ({search}) => {
     }, [search])
     console.log("STATE ", state.products)
     return(
-        <>
-           <h1>Productos a traer {search}</h1>
-           {
-               state.products.length ? 
-               state.products.map(product => (
-                   <Link to={`/product/${product.id}`}>
-                        <button>{product.title}</button>
-                   </Link>
-               ))
-               : <h1>Nada</h1>
-           }
-        </>
+        <div className="main">
+          <ul>  
+            {
+                state.products.length ? 
+                state.products.map((product, index) => (
+                    <li className="item" key={product.id}>
+                     
+                        <div className="card">
+                            <div className="image-wrapper">
+                              <Link  to={`/product/${product.id}`}>
+                                <img src={product.thumbnail}/>
+                              </Link>
+                            </div>
+                            <div className="content-wrapper">
+                                {index === 0 ? (
+                                 <div className="best-selling-tag">Mas vendido</div>
+                                ) : <></>}
+                                <Link className="item-link" key={product.id} to={`/product/${product.id}`}>
+                                        <h2 className="item-title">{product.title}</h2>
+                                </Link>
+                                <div className="item-price">
+                                    <span className="price-symbol">$</span>
+                                    <span className="price-tag">{product.price}</span>
+                                </div>
+                                <div className="item-shipping-tag">
+                                    <span>Envío gratis</span>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    
+                ))
+                : <h1>No se encontraron resultados</h1>
+            }
+          </ul>
+        </div>
 
     )
 }
