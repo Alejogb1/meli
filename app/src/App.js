906805569aeb978ad5416f7eed2561ab6550f383
Alejogb1 <52103963+Alejogb1@users.noreply.github.com>
2021-05-21 17:51:56 -0300

    ADD: Global Styles

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,17 @@
 import {useState} from "react"
 
 import Routes from "./Routes/index"
-
+import GlobalSyles from "./styles/GlobalStyles"
+import {Container} from "react-bootstrap"
+import "./styles.css"
 function App() {
   const [search, setSearch] = useState(null)
   const handlerSearch = product => setSearch(product)
   return (
-    <div className="App">
+    <Container>
       <Routes search={search} handlerSearch={handlerSearch}/>
-    </div>
+      <GlobalSyles/>
+    </Container>
   );
 }
 
